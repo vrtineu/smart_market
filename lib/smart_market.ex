1c767952f1a54b29ca19191f3375efd25f889b19
vrtineu <vininro@gmail.com>
2024-02-24 01:24:15 -0300

    initial commit

---
+++ lib/smart_market.ex
@@ -0,0 +1,18 @@
+defmodule SmartMarket do
+  @moduledoc """
+  Documentation for `SmartMarket`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SmartMarket.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/smart_market_test.exs
@@ -0,0 +1,8 @@
+defmodule SmartMarketTest do
+  use ExUnit.Case
+  doctest SmartMarket
+
+  test "greets the world" do
+    assert SmartMarket.hello() == :world
+  end
+end

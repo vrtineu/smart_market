bef43928101608c3c7a507725574a33328172229
vrtineu <vininro@gmail.com>
2024-02-24 03:00:16 -0300

    Add TransactionProcessor module to process transactions

---
+++ lib/smart_market/transaction_processor.ex
@@ -0,0 +1,13 @@
+defmodule SmartMarket.TransactionProcessor do
+  def process_transaction do
+    Task.start(fn ->
+      case SmartMarket.QueueManager.dequeue_transaction() do
+        nil ->
+          IO.puts("No transactions to process.")
+
+        transaction ->
+          IO.puts("Processing transaction: #{inspect(transaction)}")
+      end
+    end)
+  end
+end

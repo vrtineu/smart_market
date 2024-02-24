65b4b294c064bffa2a098be73804050d5daf8256
vrtineu <vininro@gmail.com>
2024-02-24 15:43:52 -0300

    Add transaction docs

--- lib/smart_market/transactions.ex
+++ lib/smart_market/transactions.ex
@@ -8,26 +8,71 @@ defmodule SmartMarket.Transactions do
   alias SmartMarket.Repo
   alias SmartMarket.Models.Transaction
 
+  @doc """
+  List all transactions.
+
+  ## Examples
+
+      iex> SmartMarket.Transactions.list_transactions()
+      [%Transaction{}, %Transaction{}]
+
+  """
   def list_transactions do
     Repo.all(Transaction)
   end
 
+  @doc """
+  Get a transaction by ID.
+
+  ## Examples
+
+      iex> SmartMarket.Transactions.get_transaction!(1)
+      %Transaction{}
+
+  """
   def get_transaction!(id) do
     Repo.get!(Transaction, id)
   end
 
+  @doc """
+  Create a transaction.
+
+  ## Examples
+
+      iex> SmartMarket.Transactions.create_transaction(%{customer_id: "1", status: "pending"})
+      %Transaction{}
+
+  """
   def create_transaction(attrs \\ %{}) do
     %Transaction{}
     |> Transaction.changeset(attrs)
     |> Repo.insert()
   end
 
+  @doc """
+  Update a transaction.
+
+  ## Examples
+
+      iex> SmartMarket.Transactions.update_transaction(%Transaction{}, %{status: "completed"})
+      %Transaction{}
+
+  """
   def update_transaction(%Transaction{} = transaction, attrs) do
     transaction
     |> Transaction.changeset(attrs)
     |> Repo.update()
   end
 
+  @doc """
+  Delete a transaction.
+
+  ## Examples
+
+      iex> SmartMarket.Transactions.delete_transaction!(%Transaction{})
+      %Transaction{}
+
+  """
   def delete_transaction!(id) do
     transaction = get_transaction!(id)
     Repo.delete!(transaction)
